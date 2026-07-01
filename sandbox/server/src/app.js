426613382257e073de3bea2f0332f0e3143db04b
Anish Upreti <upretianish407@.com>
2026-07-01 20:04:49 +0545

    setup completed

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -6,7 +6,7 @@ app.get("/" , (req , res) =>{
     res.send("HEllow world")
 })
 
-app/get("/api/sandbox/healthz" , (req , res) =>{
+app.get("/api/sandbox/healthz" , (req , res) =>{
     res.status(200).json({
         message : "Sandbox API is healthy"
     })
